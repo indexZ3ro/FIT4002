e5762fca7341851103659f3ce5530b9444a08c1a
jaiclapp <jcla0016@student.monash.edu>
2023-04-23 22:35:22 +1000

    Added ACTMatrix axis

--- client/src/Components/ACTMatrix.js
+++ client/src/Components/ACTMatrix.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./ACTMatrix.css";
 
 const ACTMatrix = () => {
-    return <div className="container"></div>;
+    return (
+        <div className="container">
+            <div>
+                <div className="line-y"></div>
+                <div className="line-x"></div>
+            </div>
+            <div className="text-inside">Inside</div>
+            <div className="text-outside">Outside</div>
+            <div className="text-towards">Towards</div>
+            <div className="text-away">Away</div>
+        </div>
+    );
 };
 
 export default ACTMatrix;
--- client/src/Components/ACTSidebar.js
+++ client/src/Components/ACTSidebar.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import './actSidebar.css';
-import SheetIcon from './DraggableIcons/SheetIcon';
-import TextIcon from './DraggableIcons/TextIcon';
-import EditIcon from './DraggableIcons/EditIcon';
-import BluePointerIcon from './DraggableIcons/BluePointerIcon';
+import React from "react";
+import "./actSidebar.css";
+import SheetIcon from "./DraggableIcons/SheetIcon";
+import TextIcon from "./DraggableIcons/TextIcon";
+import EditIcon from "./DraggableIcons/EditIcon";
+import BluePointerIcon from "./DraggableIcons/BluePointerIcon";
 
 const ACTSidebar = (props) => {
-  return (
-    <div className="actSidebar">
-        
-      <div className="draggable-container">
-        { /* Add draggable components here */ }
-        <div className="draggable-item">
-            <SheetIcon />
-        </div>
-        
-        <div className="draggable-item">
-            <TextIcon />
-        </div>
+    return (
+        <div className="actSidebar">
+            <div className="draggable-container">
+                {/* Add draggable components here */}
+                <div className="draggable-item">
+                    <SheetIcon />
+                </div>
 
-        <div className="draggable-item">
-            <EditIcon />
-        </div>
+                <div className="draggable-item">
+                    <TextIcon />
+                </div>
 
-        <div className="draggable-item">
-            <BluePointerIcon />
+                <div className="draggable-item">
+                    <EditIcon />
+                </div>
+
+                <div className="draggable-item">
+                    <BluePointerIcon />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ACTSidebar;
