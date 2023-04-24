63ba6f97cfeef8ca1ad3bee031539e9624c7fa8a
jaiclapp <jcla0016@student.monash.edu>
2023-04-24 12:39:48 +1000

    Added App component to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import NavigationStickyNote from './Components/NavigationStickyNote'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import NavigationStickyNote from "./Components/NavigationStickyNote";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <div>
-      <NavigationStickyNote />
-    </div>
-  </React.StrictMode>
+    <React.StrictMode>
+        <div>
+            <App />
+        </div>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
