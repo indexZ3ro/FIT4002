79b2e11a631f2ac81d11774deb5bcd1397de4405
jaiclapp <jcla0016@student.monash.edu>
2023-05-03 23:18:38 +1000

    Added ability to click on each quadrant and zoom

--- client/src/Components/ACTMatrix/ACTMatrix.js
+++ client/src/Components/ACTMatrix/ACTMatrix.js
@@ -2,12 +2,54 @@ import React from "react";
 import "../../css/ACTMatrix.css";
 
 const ACTMatrix = () => {
+    const quad1 = document.querySelector(".quad1");
+    const quad2 = document.querySelector(".quad2");
+    const quad3 = document.querySelector(".quad3");
+    const quad4 = document.querySelector(".quad4");
+
+    const handleClick1 = () => {
+        quad1.classList.toggle("expanded");
+        quad2.classList.toggle("hidden");
+        quad3.classList.toggle("hidden");
+        quad4.classList.toggle("hidden");
+    };
+    const handleClick2 = () => {
+        const quad1 = document.querySelector(".quad1");
+        const quad2 = document.querySelector(".quad2");
+        const quad3 = document.querySelector(".quad3");
+        const quad4 = document.querySelector(".quad4");
+        quad1.classList.toggle("hidden");
+        quad2.classList.toggle("expanded");
+        quad3.classList.toggle("hidden");
+        quad4.classList.toggle("hidden");
+    };
+    const handleClick3 = () => {
+        const quad1 = document.querySelector(".quad1");
+        const quad2 = document.querySelector(".quad2");
+        const quad3 = document.querySelector(".quad3");
+        const quad4 = document.querySelector(".quad4");
+        quad1.classList.toggle("hidden");
+        quad2.classList.toggle("hidden");
+        quad3.classList.toggle("expanded");
+        quad4.classList.toggle("hidden");
+    };
+    const handleClick4 = () => {
+        const quad1 = document.querySelector(".quad1");
+        const quad2 = document.querySelector(".quad2");
+        const quad3 = document.querySelector(".quad3");
+        const quad4 = document.querySelector(".quad4");
+        quad1.classList.toggle("hidden");
+        quad2.classList.toggle("hidden");
+        quad3.classList.toggle("hidden");
+        quad4.classList.toggle("expanded");
+    };
+
     return (
         <div className="container">
-            <div>
-                <div className="line-y"></div>
-                <div className="line-x"></div>
-            </div>
+            <div className="inner-container quad1" onClick={handleClick1}></div>
+            <div className="inner-container quad2" onClick={handleClick2}></div>
+            <div className="inner-container quad3" onClick={handleClick3}></div>
+            <div className="inner-container quad4" onClick={handleClick4}></div>
             <div className="text-inside">Inside</div>
             <div className="text-outside">Outside</div>
             <div className="text-towards">Towards</div>
