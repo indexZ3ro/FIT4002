c32d17284f230f9585f2b7b006c92e7cfdbc236f
CleshanW <cleshw@gmail.com>
2023-05-07 12:55:36 +1000

    Inital Settings Page

---
+++ client/src/pages/SettingsPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/signUpPage.css";
+import TextButton from "../Components/Buttons/TextButton";
+import "../css/createTeamMatrix.css";
+
+const SettingsPage = () => {
+
+  return (
+    <div className="SettingsPage">
+        <div className="team-matrix-top">
+            <div>Teamoji</div>
+            <div className="icon-button">
+   
+        </div>
+      </div>
+
+    </div>
+      
+  );
+};
+
+export default SettingsPage;
