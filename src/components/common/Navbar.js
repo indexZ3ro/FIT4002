0b6010f1201b996d70b9667b717325857db38de7
julius-jones <juliusjones.03.01@gmail.com>
2023-05-16 18:21:39 +1000

    commiting sidebar code

---
+++ src/components/common/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SideNav = (props) => {
+
+    return (
+        <div className = "sideNav">
+            <a href="#soloSession">Solo ACT Matrix</a>
+            <a href="#teamSession">Team Session</a>
+            <a href="#settings">Settings</a>
+        </div>
+    );
+
+
+};
+
+export default SideNav;
