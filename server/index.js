613ebc82b221917db339845d4b739eb30a107eb3
kieran moran <kmor0032@student.monash.edu>
2023-07-30 21:42:43 +1000

    Backend Integrated

--- server/index.js
+++ server/index.js
@@ -1,40 +1,45 @@
-// server/index.js
-
 const express = require("express");
-const http = require('http');
-const ShareDB = require('sharedb');
-const WebSocket = require('ws');
-const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
+const http = require("http");
+const WebSocket = require("ws");
+const WebSocketJSONStream = require("@teamwork/websocket-json-stream");
 const PORT = process.env.PORT || 8080;
 
 // Firebase initialization
-var admin = require('firebase-admin');
-const serviceAccount = require('project-5389016526708021196-firebase-adminsdk-hitz3-cab5dbb661.json');
-const db = admin.firestore();
+const admin = require("firebase-admin");
+const serviceAccount = require("../project-5389016526708021196-firebase-adminsdk-hitz3-cab5dbb661.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://project-5389016526708021196-default-rtdb.firebaseio.com"
+  databaseURL: "https://project-5389016526708021196-default-rtdb.firebaseio.com",
 });
 
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!" });
 });
-  
+
 // Create a web server to serve files and listen to WebSocket connections
 const server = http.createServer(app);
 
-// Connect any incoming WebSocket connection to ShareDB
-const wss = new WebSocket.Server({ server: server });
+// Connect any incoming WebSocket connection
+const wss = new WebSocket.Server({ server });
 
-var backend = new ShareDB();
-wss.on('connection', function(ws, req) {
-  var stream = new WebSocketJSONStream(ws);
-  backend.listen(stream);
+// Realtime Database event listeners
+const db = admin.database();
+const notesRef = db.ref("project/stickyNotes"); // Replace "stickyNotes" with your desired database path
+
+// Send data to clients when data changes in the Realtime Database
+notesRef.on("value", (snapshot) => {
+  const notesData = snapshot.val();
+  const dataToSend = Object.entries(notesData || {}).map(([key, value]) => ({ ...value, id: key }));
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(dataToSend));
+  });
+
+  console.log("Data received from Realtime Database:", dataToSend);
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
