04a0dee91a028637044aaefd125db77aa1612e68
kieran moran <kmor0032@student.monash.edu>
2023-07-31 13:05:48 +1000

    API added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const WebSocket = require("ws");
 const WebSocketJSONStream = require("@teamwork/websocket-json-stream");
 const PORT = process.env.PORT || 8080;
+const cors = require("cors");
 
 // Firebase initialization
 const admin = require("firebase-admin");
@@ -14,9 +15,95 @@ admin.initializeApp({
 });
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000"]; // Add your frontend application's URL here
+
+// CORS configuration
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Check if the origin is in the allowed origins list
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+// fetch all stickynotes
+app.get("/api/sticky-notes", (req, res) => {
+  // Fetch all sticky notes from the database
+  notesRef
+    .once("value")
+    .then((snapshot) => {
+      const notesData = snapshot.val();
+      // Convert the notesData object to an array of notes objects with IDs
+      const notesArray = Object.entries(notesData || {}).map(
+        ([key, value]) => ({ ...value, id: key })
+      );
+      res.json(notesArray);
+    })
+    .catch((error) => {
+      console.error("Error fetching sticky notes:", error);
+      res.status(500).json({ error: "Failed to fetch sticky notes" });
+    });
+});
+
+// create sticky note
+app.post("/api/sticky-notes", (req, res) => {
+  const { text, x, y } = req.body;
+  const notesRef = admin.database().ref("project/stickyNotes");
+
+  const newNoteRef = notesRef.push();
+  newNoteRef
+    .set({ text, x, y })
+    .then(() => {
+      res.status(201).json({ message: "Sticky note created successfully" });
+    })
+    .catch((error) => {
+      console.error("Error creating sticky note:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+// API route for updating an existing sticky note
+app.put("/api/sticky-notes/:noteId", (req, res) => {
+  const { noteId } = req.params;
+  const { text, x, y } = req.body;
+  const notesRef = admin.database().ref("project/stickyNotes");
+
+  notesRef
+    .child(noteId)
+    .update({ text, x, y })
+    .then(() => {
+      res.status(200).json({ message: "Sticky note updated successfully" });
+    })
+    .catch((error) => {
+      console.error("Error updating sticky note:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+// API route for deleting a sticky note
+app.delete("/api/sticky-notes/:noteId", (req, res) => {
+  const { noteId } = req.params;
+  const notesRef = admin.database().ref("project/stickyNotes");
+
+  notesRef
+    .child(noteId)
+    .remove()
+    .then(() => {
+      res.status(200).json({ message: "Sticky note deleted successfully" });
+    })
+    .catch((error) => {
+      console.error("Error deleting sticky note:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
 });
 
 // Create a web server to serve files and listen to WebSocket connections
