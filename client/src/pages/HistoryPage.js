e4e66f8f1b1a9a6d385ef122066c2020c2694752
jaiclapp <jcla0016@student.monash.edu>
2023-08-14 00:43:42 +1000

    Created initial history page

--- client/src/pages/HistoryPage.js
+++ client/src/pages/HistoryPage.js
@@ -1,9 +1,24 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import "../css/history-page.css";
+import { Sidebar } from "react-pro-sidebar";
+import SearchBar from "../Components/SearchBar";
+import HistoryTable from "../Components/HistoryTable";
 
-const HistoryPage = ({}) => {
+const HistoryPage = () => {
+    /* TODO: CHANGE HAMBURGER MENU */
     return (
         <div>
-               <h4>Hello there!</h4> 
+            <div id="header">
+                <h4>Teamoji</h4>
+            </div>
+            <div id="title-search">
+                <h2>History</h2>
+                <SearchBar></SearchBar>
+            </div>
+            <div id="main-content">
+                <HistoryTable></HistoryTable>
+            </div>
+            <div id="pages"></div>
         </div>
     );
 };
