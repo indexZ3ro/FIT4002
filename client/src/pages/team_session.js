ca83c960a8040bf78f0e3bc9b2c903359c7ffe3e
kieran moran <kmor0032@student.monash.edu>
2023-09-08 22:14:22 +1000

    Comment out team/soloSession as they are not used

--- client/src/pages/team_session.js
+++ client/src/pages/team_session.js
@@ -1,103 +1,103 @@
-import React, { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import "../App.css";
-import "../css/team-session.css";
-import Sidebar from "../Components/Sidebar/Sidebar";
-import TeamHeader from "../Components/TeamHeader/team_header";
-import ACTMatrix from "../Components/ACTMatrix/act_matrix";
-import ACTQuestions from "../Components/ACTQuestions/act_questions";
-import Timer from "../Components/Timer/Timer";
-import ACTSidebar from "../Components/ACTSidebar/act-sidebar";
-import { realtimeDb } from "../firebase";
-import { onValue, ref } from "firebase/database";
-import LocalChangeContext from "../contexts/LocalChangeContext";
-import ACTQuestionsContainer from "../Components/ACTQuestions/act_questions_container";
+// import React, { useState, useEffect, useContext } from "react";
+// import axios from "axios";
+// import "../App.css";
+// import "../css/team-session.css";
+// import Sidebar from "../Components/Sidebar/Sidebar";
+// import TeamHeader from "../Components/TeamHeader/team_header";
+// import ACTMatrix from "../Components/ACTMatrix/act_matrix";
+// import ACTQuestions from "../Components/ACTQuestions/act_questions";
+// import Timer from "../Components/Timer/Timer";
+// import ACTSidebar from "../Components/ACTSidebar/act-sidebar";
+// import { realtimeDb } from "../firebase";
+// import { onValue, ref } from "firebase/database";
+// import LocalChangeContext from "../contexts/LocalChangeContext";
+// import ACTQuestionsContainer from "../Components/ACTQuestions/act_questions_container";
 
-const TeamSession = () => {
-  const apiUrl = process.env.REACT_APP_API_URL;
-  const projectId = "1";
-  const { localChanges, setLocalChanges } = useContext(LocalChangeContext);
+// const TeamSession = () => {
+//   const apiUrl = process.env.REACT_APP_API_URL;
+//   const projectId = "1";
+//   const { localChanges, setLocalChanges } = useContext(LocalChangeContext);
 
-  // handle sticky notes state management here
-  const [notes, setNotes] = useState([]);
-  const [questions, setQuestions] = useState([]);
+//   // handle sticky notes state management here
+//   const [notes, setNotes] = useState([]);
+//   const [questions, setQuestions] = useState([]);
 
-  // Fetch all sticky notes from the database when the component mounts
-  useEffect(() => {
-    axios.get(apiUrl + `/api/project/${projectId}/sticky-notes`)
-      .then((response) => {
-        setNotes(response.data);
-        console.error("apiURL is:" + apiUrl);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("apiURL is:" + apiUrl);
-        console.error("Error fetching sticky notes:", error);
-      });
-  }, [projectId, setNotes]);
+//   // Fetch all sticky notes from the database when the component mounts
+//   useEffect(() => {
+//     axios.get(apiUrl + `/api/project/${projectId}/sticky-notes`)
+//       .then((response) => {
+//         setNotes(response.data);
+//         console.error("apiURL is:" + apiUrl);
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("apiURL is:" + apiUrl);
+//         console.error("Error fetching sticky notes:", error);
+//       });
+//   }, [projectId, setNotes]);
 
-  // Get the Question
-  useEffect(() => {
-    axios.get(apiUrl + `/api/project/${projectId}/questions`)
-      .then((response) => {
-        setQuestions(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching questions:", error);
-      });
-  }, [projectId, setQuestions]);
+//   // Get the Question
+//   useEffect(() => {
+//     axios.get(apiUrl + `/api/project/${projectId}/questions`)
+//       .then((response) => {
+//         setQuestions(response.data);
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching questions:", error);
+//       });
+//   }, [projectId, setQuestions]);
 
-  // Firebase Realtime Database listener for updates
-  useEffect(() => {
-      const notesRef = ref(realtimeDb, `Projects/${projectId}/stickyNotes`);
+//   // Firebase Realtime Database listener for updates
+//   useEffect(() => {
+//       const notesRef = ref(realtimeDb, `Projects/${projectId}/stickyNotes`);
 
-      const unsubscribe = onValue(notesRef, (snapshot) => {
-          const updatedNotes = [];
-          snapshot.forEach((childSnapshot) => {
-            const noteId = childSnapshot.key;
-            const noteData = childSnapshot.val();
-            if (localChanges.some(change => change.id === noteId)) {
-                // If the note ID is in localChanges, then retain the current note data
-                // Find the current note data
-                const currentNote = notes.find(note => note.id === noteId);
-                if (currentNote) {
-                  updatedNotes.push(currentNote);
-                  const currentTime = Date.now();
+//       const unsubscribe = onValue(notesRef, (snapshot) => {
+//           const updatedNotes = [];
+//           snapshot.forEach((childSnapshot) => {
+//             const noteId = childSnapshot.key;
+//             const noteData = childSnapshot.val();
+//             if (localChanges.some(change => change.id === noteId)) {
+//                 // If the note ID is in localChanges, then retain the current note data
+//                 // Find the current note data
+//                 const currentNote = notes.find(note => note.id === noteId);
+//                 if (currentNote) {
+//                   updatedNotes.push(currentNote);
+//                   const currentTime = Date.now();
                   
-                  setLocalChanges(prevChanges =>
-                    prevChanges.filter(change =>{
-                      const timeDifference = currentTime - change.timestamp;
-                      return !(change.id === noteId && timeDifference > 5000);
-                    })
-                  );
-                }
-            } else {
-                updatedNotes.push({ ...noteData, id: noteId });
-                // console.log("local changes: ", localChanges);
-                // console.log(noteId);
-            }
-        });
-          // Log the updated notes to the console
-          console.log("Updated notes:", updatedNotes);
-          setNotes(updatedNotes);
-      });
+//                   setLocalChanges(prevChanges =>
+//                     prevChanges.filter(change =>{
+//                       const timeDifference = currentTime - change.timestamp;
+//                       return !(change.id === noteId && timeDifference > 5000);
+//                     })
+//                   );
+//                 }
+//             } else {
+//                 updatedNotes.push({ ...noteData, id: noteId });
+//                 // console.log("local changes: ", localChanges);
+//                 // console.log(noteId);
+//             }
+//         });
+//           // Log the updated notes to the console
+//           console.log("Updated notes:", updatedNotes);
+//           setNotes(updatedNotes);
+//       });
 
-      return () => {
-          unsubscribe();
-      };
-  }, [projectId, localChanges]);
+//       return () => {
+//           unsubscribe();
+//       };
+//   }, [projectId, localChanges]);
 
-  return (
-    <div className="TeamSession">
-      <TeamHeader />
-      <ACTQuestionsContainer questions={questions}/>
-      <Sidebar />
-      <ACTMatrix notes={notes} setNotes={setNotes}/>
-      {/* <Timer /> */}
-      <ACTSidebar notes={notes} setNotes={setNotes}/>
-    </div>
-  );
-};
+//   return (
+//     <div className="TeamSession">
+//       <TeamHeader />
+//       <ACTQuestionsContainer questions={questions}/>
+//       <Sidebar />
+//       <ACTMatrix notes={notes} setNotes={setNotes}/>
+//       {/* <Timer /> */}
+//       <ACTSidebar notes={notes} setNotes={setNotes}/>
+//     </div>
+//   );
+// };
 
-export default TeamSession;
+// export default TeamSession;
