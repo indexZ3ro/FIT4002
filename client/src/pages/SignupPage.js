37cec689b03fc4e61370d1f9e3f7deafb96a2047
JaiClapp <jcla0016@student.monash.edu>
2023-09-10 21:48:01 +1000

    Fixed a typo

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -8,68 +8,71 @@ import TextButton from "../Components/Buttons/textButton";
 import SignUpPageCover from "../assets/signUpPageCover.svg";
 
 const SignUpPage = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(hideSideBar());
-  });
+    useEffect(() => {
+        dispatch(hideSideBar());
+    });
 
-  const signUp = () => {
-    navigate("/Home");
-  };
+    const signUp = () => {
+        navigate("/Home");
+    };
 
-  const logIn = () => {
-    navigate("/LogIn");
-  };
+    const logIn = () => {
+        navigate("/LogIn");
+    };
 
-  return (
-    <div className="signUpPage">
-      <div className="split-left-signup">
-        <div className="signUpPageTeam">Teamoji</div>
-        <div className="signUpPageCover">
-          <img className="signUpPageCoverImg" src={SignUpPageCover}></img>
+    return (
+        <div className="signUpPage">
+            <div className="split-left-signup">
+                <div className="signUpPageTeam">Teamoji</div>
+                <div className="signUpPageCover">
+                    <img
+                        className="signUpPageCoverImg"
+                        src={SignUpPageCover}
+                    ></img>
+                </div>
+            </div>
+            <div className="split-right-signup">
+                <div className="signUpTitle">Sign Up</div>
+                <div className="inputContainer">
+                    <input
+                        className="userInput text"
+                        placeholder="Email"
+                        type="text"
+                    ></input>
+                    <input
+                        className="userInput text "
+                        type="text"
+                        placeholder="Username"
+                    ></input>
+                    <input
+                        className="userInput text"
+                        placeholder="Name"
+                        type="text"
+                    ></input>
+                    <input
+                        className="userInput text"
+                        type="password"
+                        placeholder="Password"
+                    ></input>
+                    <TextButton
+                        id="Sign Up"
+                        customStyle={{
+                            width: "15vw",
+                            height: "5vh",
+                            background: "#EFDFFD",
+                        }}
+                        handleClick={signUp}
+                    />
+                    <div className="signUpToLogin" onClick={logIn}>
+                        Already have an account?
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="split-right-signup">
-        <div className="signUpTitle">Sign Up</div>
-        <div className="inputContainer">
-          <input
-            className="userInput text"
-            placeholder="Email"
-            type="text"
-          ></input>
-          <input
-            className="userInput text "
-            type="text"
-            placeholder="Username"
-          ></input>
-          <input
-            className="userInput text"
-            placeholder="Name"
-            type="text"
-          ></input>
-          <input
-            className="userInput text"
-            type="password"
-            placeholder="Password"
-          ></input>
-          <TextButton
-            id="signUp"
-            customStyle={{
-              width: "15vw",
-              height: "5vh",
-              background: "#EFDFFD",
-            }}
-            handleClick={signUp}
-          />
-          <div className="signUpToLogin" onClick={logIn}>
-            Already have an account?
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SignUpPage;
