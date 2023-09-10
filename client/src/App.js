3d7602279512b6297c2fdc678a66ba3981ee4a88
JaiClapp <jcla0016@student.monash.edu>
2023-09-11 00:13:39 +1000

    Added basic about us page

--- client/src/App.js
+++ client/src/App.js
@@ -16,39 +16,60 @@ import LocalChangeContext from "./contexts/LocalChangeContext";
 import QuestionContext from "./contexts/QuestionContext";
 import React, { useRef, useState } from "react";
 import HistoryPage from "./pages/history_page";
-import Settings from './pages/settings_page'
-import InfiniteCanvas from './pages/infiniteCanvas';
+import Settings from "./pages/settings_page";
+import AboutUs from "./pages/about_us";
+import InfiniteCanvas from "./pages/infiniteCanvas";
 
 function App() {
-  const [localChanges, setLocalChanges] = useState([]);
-  const [localQuestions, setLocalQuestions] = useState([]);
-  return (
-    <Provider store={store}>
-      <LocalChangeContext.Provider value={{ localChanges, setLocalChanges }}>
-        <QuestionContext.Provider value={{ localQuestions, setLocalQuestions }}>
-          <Router>
-            <div className="App">
-              <Sidebar />
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/LogIn" element={<LogInPage />} />
-                <Route path="/Home" element={<Homepage />} />
-                <Route path="/Signup" element={<SignUpPage />} />
-                <Route
-                  path="/CreateTeamMatrix"
-                  element={<CreateTeamMatrix />}
-                />
-                <Route path="/SoloSession" element={<InfiniteCanvas />} />
-                <Route path="/TeamSession" element={<InfiniteCanvas />} />
-                <Route path="/HistoryPage" element={<HistoryPage />} />
-                <Route path="/Settings" element={<Settings />} />
-              </Routes>
-            </div>
-          </Router>
-        </QuestionContext.Provider>
-      </LocalChangeContext.Provider>
-    </Provider>
-  );
+    const [localChanges, setLocalChanges] = useState([]);
+    const [localQuestions, setLocalQuestions] = useState([]);
+    return (
+        <Provider store={store}>
+            <LocalChangeContext.Provider
+                value={{ localChanges, setLocalChanges }}
+            >
+                <QuestionContext.Provider
+                    value={{ localQuestions, setLocalQuestions }}
+                >
+                    <Router>
+                        <div className="App">
+                            <Sidebar />
+                            <Routes>
+                                <Route path="/" element={<LandingPage />} />
+                                <Route path="/LogIn" element={<LogInPage />} />
+                                <Route path="/Home" element={<Homepage />} />
+                                <Route
+                                    path="/Signup"
+                                    element={<SignUpPage />}
+                                />
+                                <Route path="/AboutUs" element={<AboutUs />} />
+                                <Route
+                                    path="/CreateTeamMatrix"
+                                    element={<CreateTeamMatrix />}
+                                />
+                                <Route
+                                    path="/SoloSession"
+                                    element={<InfiniteCanvas />}
+                                />
+                                <Route
+                                    path="/TeamSession"
+                                    element={<InfiniteCanvas />}
+                                />
+                                <Route
+                                    path="/HistoryPage"
+                                    element={<HistoryPage />}
+                                />
+                                <Route
+                                    path="/Settings"
+                                    element={<Settings />}
+                                />
+                            </Routes>
+                        </div>
+                    </Router>
+                </QuestionContext.Provider>
+            </LocalChangeContext.Provider>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ client/src/pages/about_us.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { hideSideBar } from "../features/sidebarSlice";
+import team from "../assets/team.png";
+const AboutUs = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const home = () => {
+        navigate("/Home");
+    };
+
+    useEffect(() => {
+        dispatch(hideSideBar());
+    });
+
+    return (
+        <div className="team-img-container">
+            <img
+                className="team-img"
+                src={team}
+                style={{ width: 100 + "vh", height: "auto" }}
+            ></img>
+        </div>
+    );
+};
+
+export default AboutUs;
