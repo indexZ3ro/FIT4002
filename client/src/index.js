de6000a6ff9b5edd6414f17792e8107ceb133b61
JaiClapp <jcla0016@student.monash.edu>
2023-10-07 22:31:24 +1100

    Enabled mocking

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import NavigationStickyNote from "./Components/NavigationStickyNote/NavigationStickyNote";
-
+require("./mocks/browser.js");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
