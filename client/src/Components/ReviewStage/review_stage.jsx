277b79a5368100870b9cd47b5e032b90cf1ba795
kieran moran <kmor0032@student.monash.edu>
2023-10-08 13:24:02 +1100

    Review stage Modal

--- client/src/Components/ReviewStage/review_stage.jsx
+++ client/src/Components/ReviewStage/review_stage.jsx
@@ -1,22 +1,56 @@
-import React from 'react';
-import locationPin from '../../assets/locationPin.svg'; // Ensure correct path to your asset
+import React, { useState } from 'react';
+import locationPin from '../../assets/locationPin.svg';
 
-const ReviewStage = ({ onClose }) => { // Added onClose prop to close the modal
+const ReviewStage = () => {
+    const [isDragging, setIsDragging] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(true);
+
+    const startDragging = (e) => {
+        e.preventDefault();
+        setIsDragging(true);
+        setIsModalVisible(false);
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', stopDragging);
+    };
+
+    const onMouseMove = (e) => {
+        const img = document.querySelector('.reviewPin');
+        img.style.left = e.pageX - img.width / 2 + 'px';
+        img.style.top = e.pageY - img.height / 2 + 'px';
+    };
+
+    const stopDragging = () => {
+        setIsDragging(false);
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', stopDragging);
+    };
+
     return (
-        <div className="timerModal">
-            <div className="timerWrap">
-                <div class="whiteTextbox">
-                    <div className='centreText'>Let's review your matrix.</div>
-                    <div className='centreText'>Pick up this pin and place it on the matrix where you think you're currently at.</div>
+        <>
+            {isModalVisible && (
+                <div className="timerModal">
+                    <div className="timerWrap">
+                        <div className="whiteTextbox">
+                            <div className='centreText'>Let's review your matrix.</div>
+                            <div className='centreText'>Pick up this pin and place it on the matrix where you think you're currently at.</div>
+                        </div>
+                    </div>
                 </div>
-                <img
-                    className="reviewPin whiteTextbox"
-                    src={locationPin}
-                    alt="Review Pin" 
-                    style={{ cursor: 'pointer' }}
-                />
-            </div>
-        </div>
+            )}
+            <img
+                className="reviewPin whiteTextbox"
+                src={locationPin}
+                alt="Review Pin"
+                style={{
+                    cursor: 'grab',
+                    position: 'fixed',
+                    top: '60%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)' // Added this to ensure the image is centered
+                }}
+                onMouseDown={startDragging}
+            />
+        </>
     );
 };
 
