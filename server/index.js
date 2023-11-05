8dbb3ff6a6c048944a15f7ea8540b42bda2a9aca
kieran moran <kmor0032@student.monash.edu>
2023-11-06 10:09:23 +1100

    deployment

--- server/index.js
+++ server/index.js
@@ -816,27 +816,8 @@ app.post("/api/update-user-review", async (req, res) => {
 // Create a web server to serve files and listen to WebSocket connections
 const server = http.createServer(app);
 
-// Unused
 // Connect any incoming WebSocket connection
 const wss = new WebSocket.Server({ server });
-
-// // Realtime Database event listeners
-// const db = admin.database();
-// const notesRef = db.ref("project/stickyNotes");
-
-// // Send data to clients when data changes in the Realtime Database
-// notesRef.on("value", (snapshot) => {
-//   const notesData = snapshot.val();
-//   const dataToSend = Object.entries(notesData || {}).map(([key, value]) => ({
-//     ...value,
-//     id: key,
-//   }));
-//   wss.clients.forEach((client) => {
-//     client.send(JSON.stringify(dataToSend));
-//   });
-
-//   console.log("Data received from Realtime Database:", dataToSend);
-// });
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
